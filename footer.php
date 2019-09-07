dedb31ee7e698a6711e8e1daf225ed1c3614b0f5
Halkawt mahdi <hm15313@auis.edu.krd>
2019-09-07 15:16:29 +0300

    new fixes

--- footer.php
+++ footer.php
@@ -26,12 +26,6 @@ function showPage() {
   document.getElementById("myDiv").style.display = "block";
 }
 </script>
-<?php
-}
-else {
-    echo "May you please switch to A device browser!";
-}
-?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
--- header.php
+++ header.php
@@ -11,10 +11,4 @@
   <link rel="stylesheet" href="animate.css">
   <link rel="stylesheet" href="style.css">
 </head>
-<?php
-function isMobileDevice() {
-    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
-}
-if(isMobileDevice()){
-    ?>
 <body onload="myFunction()">
